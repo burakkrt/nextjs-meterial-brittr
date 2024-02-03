5c6faee253623e1bea9e8d4981e200b3e969850b
burakkrt <burak.kurt@glomil.com>
2024-02-03 08:43:18 +0300

    create step 1

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit dolorum illo in odio, magni, nam
+      nihil non, distinctio est quasi rerum necessitatibus! Assumenda ipsa itaque sed molestias.
+      Quae, veniam. Exercitationem!
+    </div>
+  );
+};
+
+export default page;
