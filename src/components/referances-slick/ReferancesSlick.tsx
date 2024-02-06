231fbea399328fd97ce7270dfad18368673beed3
burakkrt <burak.kurt@glomil.com>
2024-02-06 18:38:14 +0300

    referances compalted

--- src/components/referances-slick/ReferancesSlick.tsx
+++ src/components/referances-slick/ReferancesSlick.tsx
@@ -3,47 +3,107 @@ import React from 'react';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
+import Image from 'next/image';
 
 const ReferancesSlick = () => {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 8,
+    slidesToScroll: 1,
     autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 4000,
-    // cssEase: 'linear',
+    speed: 500,
+    autoplaySpeed: 2000,
+    initialSlide: 0,
+    pauseOnHover: false,
+    cssEase: 'linear',
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          speed: 3000,
+          autoplaySpeed: 3000,
+          autoplay: false,
+        },
+      },
+    ],
   };
+
+  function SampleNextArrow(props: any) {
+    const { className, onClick } = props;
+    return <div className={className} onClick={onClick}></div>;
+  }
+
+  function SamplePrevArrow(props: any) {
+    const { className, onClick } = props;
+    return <div className={className} onClick={onClick} />;
+  }
+
   return (
     <div className="slider-container">
       <Slider {...settings}>
-        <div>
-          <h3>1</h3>
+        <div className="slick-item">
+          <div className="ref-image">
+            <Image src="/images/referances/ref-1.jpg" alt="Referance Logo" fill />
+          </div>
         </div>
-        <div>
-          <h3>2</h3>
+        <div className="slick-item">
+          <div className="ref-image">
+            <Image src="/images/referances/ref-2.jpg" alt="Referance Logo" fill />
+          </div>
         </div>
-        <div>
-          <h3>3</h3>
+        <div className="slick-item">
+          <div className="ref-image">
+            <Image src="/images/referances/ref-3.png" alt="Referance Logo" fill />
+          </div>
         </div>
-        <div>
-          <h3>4</h3>
+        <div className="slick-item">
+          <div className="ref-image">
+            <Image src="/images/referances/ref-4.png" alt="Referance Logo" fill />
+          </div>
         </div>
-        <div>
-          <h3>5</h3>
+        <div className="slick-item">
+          <div className="ref-image">
+            <Image src="/images/referances/ref-5.jpg" alt="Referance Logo" fill />
+          </div>
         </div>
-        <div>
-          <h3>6</h3>
+        <div className="slick-item">
+          <div className="ref-image">
+            <Image src="/images/referances/ref-6.jpg" alt="Referance Logo" fill />
+          </div>
         </div>
-        <div>
-          <h3>7</h3>
+        <div className="slick-item">
+          <div className="ref-image">
+            <Image src="/images/referances/ref-7.png" alt="Referance Logo" fill />
+          </div>
         </div>
-        <div>
-          <h3>8</h3>
+        <div className="slick-item">
+          <div className="ref-image">
+            <Image src="/images/referances/ref-8.png" alt="Referance Logo" fill />
+          </div>
         </div>
-        <div>
-          <h3>9</h3>
+        <div className="slick-item">
+          <div className="ref-image">
+            <Image src="/images/referances/ref-9.png" alt="Referance Logo" fill />
+          </div>
+        </div>
+        <div className="slick-item">
+          <div className="ref-image">
+            <Image src="/images/referances/ref-10.png" alt="Referance Logo" fill />
+          </div>
         </div>
       </Slider>
     </div>
