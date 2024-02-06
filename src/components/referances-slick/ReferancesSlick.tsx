899aafe83ff15bbca90cd2dd3fc3c1a6689be028
burakkrt <burak.kurt@glomil.com>
2024-02-06 22:59:20 +0300

    referances responsive

--- src/components/referances-slick/ReferancesSlick.tsx
+++ src/components/referances-slick/ReferancesSlick.tsx
@@ -21,22 +21,33 @@ const ReferancesSlick = () => {
     prevArrow: <SamplePrevArrow />,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1700,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
+          slidesToShow: 7,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 1440,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          speed: 3000,
-          autoplaySpeed: 3000,
-          autoplay: false,
+          slidesToShow: 6,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
         },
       },
     ],
