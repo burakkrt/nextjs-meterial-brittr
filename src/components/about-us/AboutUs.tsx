013b2602630105232e452d608981ff7d4a73541e
burakkrt <burak.kurt@glomil.com>
2024-02-06 20:36:42 +0300

    ..

---
+++ src/components/about-us/AboutUs.tsx
@@ -0,0 +1,40 @@
+'use client';
+import { Container } from '@mui/material';
+import Image from 'next/image';
+import React from 'react';
+import { useParallax } from 'react-scroll-parallax';
+
+const AboutUs = () => {
+  const { ref: parallaxRefTitle } = useParallax({
+    speed: -30,
+  });
+
+  const { ref: parallaxRefImage } = useParallax({
+    translateX: ['300px', '0px'],
+  });
+
+  return (
+    <div className="about-us">
+      <Container maxWidth="lg" className="about-us-content">
+        <div className="about-us-content-view" ref={parallaxRefTitle as any}>
+          <div className="about-us-content-view-left">
+            <h2 className="about-us-content-view-left-title">Biz kimiz ?</h2>
+            <p className="about-us-content-view-left-text">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum eum aliquam aliquid
+              impedit optio labore nostrum porro vitae ex explicabo blanditiis dolorem assumenda
+              recusandae quaerat, necessitatibus soluta alias enim perspiciatis? Officiis
+              consequatur sit iusto commodi tempore exercitationem voluptates reiciendis fugit
+              corrupti voluptas nam eligendi inventore repudiandae sed soluta quam et debitis ut
+              dolorum ad, expedita quis. Fugiat consequuntur hic omnis.
+            </p>
+          </div>
+          <div className="about-us-content-view-image" ref={parallaxRefImage as any}>
+            <Image src="/images/about-us-why.png" alt="About Us Image" fill />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default AboutUs;
--- src/containers/home/page.tsx
+++ src/containers/home/page.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { IRootParams } from './types';
 import WelcomeBanner from '@/components/welcome-banner';
 import ReferancesSlick from '@/components/referances-slick';
+import AboutUs from '@/components/about-us';
 
 function HomeContainer({ locale }: IRootParams) {
   return (
     <>
       <WelcomeBanner />
       <ReferancesSlick />
+      <AboutUs />
     </>
   );
 }
