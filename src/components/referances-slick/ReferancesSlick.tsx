5a044d0709e06bef479a32833e774c9e290973ec
burakkrt <burak.kurt@glomil.com>
2024-02-06 15:20:01 +0300

    add slick referances

---
+++ src/components/referances-slick/ReferancesSlick.tsx
@@ -0,0 +1,53 @@
+'use client';
+import React from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+
+const ReferancesSlick = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 4000,
+    // cssEase: 'linear',
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+        <div>
+          <h3>7</h3>
+        </div>
+        <div>
+          <h3>8</h3>
+        </div>
+        <div>
+          <h3>9</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default ReferancesSlick;
--- src/containers/home/page.tsx
+++ src/containers/home/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { IRootParams } from './types';
 import WelcomeBanner from '@/components/welcome-banner';
+import ReferancesSlick from '@/components/referances-slick';
 
 function HomeContainer({ locale }: IRootParams) {
   return (
     <>
       <WelcomeBanner />
+      <ReferancesSlick />
     </>
   );
 }
