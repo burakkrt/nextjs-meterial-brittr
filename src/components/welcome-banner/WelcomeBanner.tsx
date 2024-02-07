8b9f886e885d039bd017d334fb05f2bb3450064c
burakkrt <burak.kurt@glomil.com>
2024-02-07 16:18:55 +0300

    video banner modified

--- src/components/welcome-banner/WelcomeBanner.tsx
+++ src/components/welcome-banner/WelcomeBanner.tsx
@@ -10,21 +10,21 @@ const WelcomeBanner = () => {
     loop: true,
   };
 
-  const parallax = useParallax<HTMLDivElement>({ speed: -15 });
+  // const parallax = useParallax<HTMLDivElement>({ speed: -15 });
 
   return (
     <div className="welcome-banner">
       <video {...controls} className="welcome-banner-video">
-        <source src="videos/welcome-video-v2.mp4" type="video/mp4" />
+        <source src="videos/video-banner-back.mp4" type="video/mp4" />
       </video>
-      <div className="welcome-banner-content" ref={parallax.ref}>
+      {/* <div className="welcome-banner-content" ref={parallax.ref}>
         <h1 className="welcome-banner-content-title">Lorem ipsum dolor sit amet.</h1>
         <p className="welcome-banner-content-text">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, excepturi quas, nam
           numquam ullam ipsa minima quisquam earum maxime labore laboriosam vitae. Illo numquam
           omnis eligendi provident optio unde odio.
         </p>
-      </div>
+      </div> */}
       <KeyboardDoubleArrowDownIcon className="welcome-banner-down-icon" />
     </div>
   );
