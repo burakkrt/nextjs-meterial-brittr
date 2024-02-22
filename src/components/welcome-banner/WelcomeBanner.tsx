0addc91ecd194ea2a3978433895439ac7ce09a4e
burakkrt <burak.kurt@glomil.com>
2024-02-22 21:19:23 +0300

    add poster

--- src/components/welcome-banner/WelcomeBanner.tsx
+++ src/components/welcome-banner/WelcomeBanner.tsx
@@ -9,20 +9,27 @@ import { isMobile } from 'react-device-detect';
 const WelcomeBanner = () => {
   const t = useTranslations('Welcome');
 
-  function renderVideoDevice(mobileCheck: boolean) {
+  function renderVideo(mobileCheck: boolean) {
     if (mobileCheck) {
-      return <source src="videos/welcome-video-mobile.mp4" />;
+      return (
+        <Player autoPlay muted playsInline fluid poster="/images/welcome-poster-mobile.png">
+          <source src="/videos/welcome-video-mobile.mp4" />
+          <ControlBar autoHide={true} className="video-controls-bar" />
+        </Player>
+      );
     } else {
-      return <source src="videos/welcome-video-back.mp4" />;
+      return (
+        <Player autoPlay muted playsInline fluid poster="/images/welcome-poster.png">
+          <source src="/videos/welcome-video-back.mp4" />
+          <ControlBar autoHide={true} className="video-controls-bar" />
+        </Player>
+      );
     }
   }
 
   return (
     <div className="welcome-banner" id="welcomeBanner">
-      <Player autoPlay muted playsInline fluid>
-        {renderVideoDevice(isMobile)}
-        <ControlBar autoHide={true} className="video-controls-bar" />
-      </Player>
+      {renderVideo(isMobile)}
       <div className="welcome-banner-content">
         <h1 className="welcome-banner-content-title">{t('videoTitle')}</h1>
         <p className="welcome-banner-content-text">{t('videoDesc')}</p>
